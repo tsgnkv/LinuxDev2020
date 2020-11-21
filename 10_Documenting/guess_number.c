c90f2cd728145a92679108431a3cf35f2684e5b1
Aleksandr Tsygankov <atsygankov@hotmail.com>
2020-11-21 12:45:48 +0000

    add doxygen

--- 10_Documenting/guess_number.c
+++ 10_Documenting/guess_number.c
@@ -1,3 +1,9 @@
+/** @file guess_number.c
+ * Main file
+ *
+ * Guessing number game 
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <libgen.h>
@@ -7,6 +13,25 @@
 
 #define _(STRING) gettext(STRING)
 #define LOCALE_PATH "."
+
+
+/**
+ * Convert an integer number @p int_number to its roman representation 
+ * @param int_number should be a number from range [1..100] 
+ * @returns roman string or NULL value
+ */
+char * int_to_roman(int int_number) {
+  return NULL;
+}
+
+/**
+ * Convert a string @p roman_number to its integer representation 
+ * @param roman_number is a string
+ * @returns integer value in range [1..100] or -1
+ */
+int roman_to_int(char *roman_number) {
+  return -1;
+}
 
 int main(int argc, char *argv[]) {
   setlocale (LC_ALL, "");
