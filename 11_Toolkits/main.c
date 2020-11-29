58580eda2880c525e80b2f377d2ca083f49783b8
Aleksandr Tsygankov <atsygankov@hotmail.com>
2020-11-29 10:35:13 +0000

    only counter

---
+++ 11_Toolkits/main.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <glib.h>
+
+const int MAX_LENGTH = 80;
+const char* DELIMETER  = " ";
+
+gboolean finder(gpointer key, gpointer value, gpointer user_data) {
+	return !g_ascii_strcasecmp(key, user_data);
+}
+
+void print(gpointer key, gpointer value, gpointer user_data) {
+ printf("%s\t%d\n", (char* )key, *(gint*)value);
+}
+
+int main(int argc, char** argv) {
+  if (argc != 2) {
+    printf("Error: Usage %s filename\n", argv[0]);
+    return 1;
+  }
+
+  FILE* file = fopen(argv[1], "r");
+
+  if ((file = fopen(argv[1], "r")) == NULL) {
+    printf("Error: Can't open file %s\n", argv[1]);
+    return 1;
+  }
+
+  GHashTable* counter = g_hash_table_new(g_str_hash, g_str_equal);
+  
+  gchar line[MAX_LENGTH + 1];
+
+  while (fgets(line, MAX_LENGTH + 1, file)) {
+    line[strlen(line) - 1] = '\0';
+    gchar** words = g_strsplit(line, DELIMETER, -1);
+    for (int i = 0; words[i]; ++i) {
+      gpointer item_ptr = g_hash_table_find(counter, (GHRFunc)finder, words[i]);
+      if (item_ptr) {
+        // find key
+        (*(gint *)item_ptr)++;
+      } else {
+        // not find key
+        gint* tmp = g_new(gint, 1);
+        *tmp = 1;
+        gchar* strtmp = g_strdup(words[i]);
+        g_hash_table_insert(counter, strtmp, tmp);
+      }
+    }
+    g_strfreev(words); 
+	}
+
+  g_hash_table_foreach(counter, print, NULL);
+  g_hash_table_destroy(counter);
+  return 0;
+}
