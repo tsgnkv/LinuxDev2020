6bd012258f1f57925258ccdff24926ebd60d34d1
Aleksandr Tsygankov <atsygankov@hotmail.com>
2020-12-04 21:28:38 +0000

    task 12

---
+++ 12_ScriptingText/fun.c
@@ -0,0 +1,13 @@
+/* COMMENT:: prog, 0.0.1: Fri Dec  4 21:25:41 UTC 2020 */
+#include <stdio.h>
+#include "outlib.h"
+
+void output(char *str) {
+  printf("%d: %s\012", Count++, str);
+}
+
+void usage(char *prog) {
+  fprintf(stderr, "%s v%.2f: Print all arguments\012\t"\
+          "Usage: %s arg1 [arg2 […]]\012", prog, VERSION, prog);
+}
+
---
+++ 12_ScriptingText/outlib.h
@@ -0,0 +1,7 @@
+/* COMMENT:: prog, 0.0.1: Fri Dec  4 21:25:41 UTC 2020 */
+void output(char *);
+void usage(char *);
+extern int Count;
+
+#define VERSION 0.0
+
